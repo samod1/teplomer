0b8e66e5ec18e783ce9b834a8973489065ca105a
Samuel <samko.domin@gmail.com>
2022-10-14 10:57:20 +0200

    Update okamzitaTeplota.py

--- okamzitaTeplota.py
+++ okamzitaTeplota.py
@@ -18,21 +18,21 @@ while True:
                 text = '';
                 while text.split("\n")[0].find("YES") == -1 :
 			# Open the file that we viewed earlier so that python can see what is in it. Replace the serial number as before.
-			tfilename = "/sys/bus/w1/devices/" + sens +"/w1_slave"
-			tfile = open(tfilename)
+                        tfilename = "/sys/bus/w1/devices/" + sens +"/w1_slave"
+                        tfile = open(tfilename)
 			# Read all of the text in the file.
-			text = tfile.read()
+                        text = tfile.read()
 			# Close the file now that the text has been read.
-			tfile.close()
-			time.sleep(0.1)
+                        tfile.close()
+                        time.sleep(0.1)
 			# Split the text with new lines (\n) and select the second line.
-			secondline = text.split("\n")[1]
+                        secondline = text.split("\n")[1]
 			# Split the line into words, referring to the spaces, and select the 10th word (counting from 0).
-			temperaturedata = secondline.split(" ")[9]
+                        temperaturedata = secondline.split(" ")[9]
 			# The first two characters are "t=", so get rid of those and convert the temperature from a string to a number.
-			temperature = float(temperaturedata[2:])
-			print("okamzita teplota zo senzora "+ sens+"je:",temperature/1000)
-			lines.append(sens+ " "+str(temperature/1000))
+                        temperature = float(temperaturedata[2:])
+                        print("okamzita teplota zo senzora "+ sens+"je:",temperature/1000)
+                        lines.append(sens+ " "+str(temperature/1000))
 
 
 	temps.write(lines[0])
